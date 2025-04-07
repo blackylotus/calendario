abaa986e55fcc541bca96c1cce24e1b130a470da
blackylotus <felipebaristacontacto@gmail.com>
2025-04-06 21:33:59 -0400

    pruebas php

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,9 +1,32 @@
 <?php 
 echo "prueba1";
 
-  $enlace= mysqli_connect("localhost", "root", "root", "reservar_citas")
 
-  if(!$enlace) {
+
+$conn->$query(CREATE DATABASE IF NOT EXISTS, "reservar_citas2")
+
+
+
+  
+
+
+
+// mención, investigando encontré que $enlace, es el nombre atribuido a la conexión
+$enlace= mysqli_connect("localhost", "root", "root", "reservar_citas")
+
+$result = mysqli_query($enlace, $query);
+
+/https://www.php.net/manual/es/mysqli.query.php
+if (mysqli_query($enlace, "CREATE TEMPORARY DATABASE reservar_citas LIKE citas") === TRUE) {
+    echo ("Se creó con éxtio la tabla reservar_citas.\n");
+
+/correccion
+if <mysqli->
+    <query></query>
+</mysqli->
+
+
+if(!$enlace) {
     echo "error en codigo enlace linea 4"  . PHP_EOL;
     echo "prueba" . mysqli_connect_errno() . PHP_EOL;
     echo "error" . mysqli_connect_error()  . PHP EOL;
