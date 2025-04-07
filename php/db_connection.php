62e5cf0646bfe7918deb8687fa6c1e0bbdf5d78d
blackylotus <felipebaristacontacto@gmail.com>
2025-04-06 21:13:26 -0400

    actualizacion php

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,3 +1,18 @@
 <?php 
 echo "prueba1";
 
+  $enlace= mysqli_connect("localhost", "root", "root", "reservar_citas")
+
+  if(!$enlace) {
+    echo "error en codigo enlace linea 4"  . PHP_EOL;
+    echo "prueba" . mysqli_connect_errno() . PHP_EOL;
+    echo "error" . mysqli_connect_error()  . PHP EOL;
+
+   }
+
+   echo "conexcion completa" . PHP_EOL;
+   echo "prueba: "  . mysqli_get_host_info($enlace) . PHP EOL;
+
+   mysqli_close($enlace);
+
+   ?>
